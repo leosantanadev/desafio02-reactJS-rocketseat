283501ed22c3f8ef718b282a3811aa1312aa0d57
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2021-07-20 14:45:40 -0300

    :boom: Challenge Completed :boom:

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
 }
+
+export function SideBar({genres, selectedGenreId, handleClickButton} : SideBarProps) {
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+      
+    </nav>
+  )
+}
+
+// Puta que pariu leozao 
+// Parabens mano conseguiu fazer o desafio 
+// Mais uma conquista pessoal!! Só PROGRESSO!!!...
